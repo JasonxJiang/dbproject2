66c71ce09b9bac75739b80a67374b8f7b5023c96
Jason Jiang <jiang.jason.x@gmail.com>
2016-10-16 14:16:02 -0700

    Styled data display live refresh button added

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
 
 $(document).ready(function(){
+	getData();
+	$("button").click(function(){
+		getData();
+	})
+});
+
+function getData() {
 	$.ajax({
-		url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson",
+		url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
 		context: document.body
 	}).done(function(data){
 		console.log(data);
 		parseData(data);
 	});
-});
+}
 
 function parseData(data) {
 	var source = $("#earthquake-template").html();
 	var template = Handlebars.compile(source);
 	$(".earthquake-content").html(template(data));
-
 }
+
